745c2d48425b758776407f4951e7cf926bbc4f25
JJ <jonathanjamesholt@gmail.com>
2026-06-21 03:49:25 +0100

    WIP: All structures are flattened

--- @IPathAverage/split_flex_ext.m
+++ @IPathAverage/split_flex_ext.m
@@ -1,26 +1,27 @@
 function [flex, ext] = split_flex_ext(self)
-    error("Not yet implemented")
-    directions = self.Directions;
-    states = self.States;
-    signals = self.Signals;
-
     flex = self;
     ext = self;
 
-    for st = 1:numel(states)
-        state = states(st);
-        for sg = 1:numel(signals)
-            signal = signals(sg);
-            datum = self.Kinematics.(state).(signal);
-            headers = datum.Properties.VariableNames;
-            is_flexion = strcmpi(headers, 'flexion');
-            flexion = headers{is_flexion};
-            [~, n] = max(flexion);
+    [flex.Kinematics] = deal([]);
+    [ext.Kinematics] = deal([]);
 
-            flex.Kinematics.(state).(signal).mean = datum.mean(1:n, :);
-            flex.Kinematics.(state).(signal).std = datum.std(1:n, :);
-            ext.Kinematics.(state).(signal).mean = datum.mean(n:end, :);
-            ext.Kinematics.(state).(signal).std = datum.std(n:end, :);
+    for n = 1:numel(self)
+        kinematics = self(n).Kinematics;
+        signals = fields(kinematics);
+        for sg = 1:numel(signals)
+            signal = signals{sg};
+            datum = kinematics.(signal);
+
+            if isempty(datum)
+                continue
+            end
+
+            [~, i] = max(datum.flexion);
+            flex(n).Kinematics.(signal) = datum(1:i, :);
+            flex(n).Stdev.(signal) = self(n).Stdev.(signal)(1:i, :);
+
+            ext(n).Kinematics.(signal) = datum(i:end, :);
+            ext(n).Stdev.(signal) = self(n).Stdev.(signal)(1:i, :);
         end
     end
 end
