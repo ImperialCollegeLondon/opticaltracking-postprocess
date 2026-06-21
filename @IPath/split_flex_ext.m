f2958e069e281aa57937955a31c8c4b3f0281156
JJ <jonathanjamesholt@gmail.com>
2026-06-21 03:48:46 +0100

    Moved functions to interface

---
+++ @IPath/split_flex_ext.m
@@ -0,0 +1,24 @@
+function [flex, ext] = split_flex_ext(self)
+    flex = self;
+    ext = self;
+
+    [flex.Kinematics] = deal([]);
+    [ext.Kinematics] = deal([]);
+
+    for n = 1:numel(self)
+        kinematics = self(n).Kinematics;
+        signals = fields(kinematics);
+        for sg = 1:numel(signals)
+            signal = signals{sg};
+            datum = kinematics.(signal);
+
+            if isempty(datum)
+                continue
+            end
+
+            [~, i] = max(datum.flexion);
+            flex(n).Kinematics.(signal) = datum(1:i, :);
+            ext(n).Kinematics.(signal) = datum(i:end, :);
+        end
+    end
+end
